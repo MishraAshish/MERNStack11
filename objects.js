410fdcc2f232e29706a3e2b3cfd021f74db2c8aa
AshisMishra <ash.ishu025@gmail.com>
2022-08-09 20:45:01 +0530

    object

--- objects.js
+++ objects.js
@@ -12,7 +12,7 @@ var User = {
     }
 }
 
-console.log(User.getUserName())
+//console.log(User.getUserName())
 
 //inheritance 
 //1. by using the new keyword
@@ -45,5 +45,37 @@ student.getUserName = function (params) { //overriding the method/functions
 
 //console.log(student.age)
 //console.log(student.getAddress())
-console.log(student.getUserName())
-console.log(User.getUserName)
+//console.log(student.getUserName())
+//console.log(User.getUserName)
+
+//console.log("Student ", student.__proto__)
+//console.log("Student ", student.__proto__.__proto__)
+//console.log("User ", User.__proto__)
+
+
+//3. Merging of objects
+
+// var mergeObj = {User, Address, Product, Shipping} // references keep on updating so not a great way
+
+var obj1 = {name : "MyName", age : 29, salary : 250}
+var obj2 = {name : "YourName", address:"New City ", skills : "Javascript"}
+var obj3 = {name : "MyName3", session:"MERNStack"}
+
+Object.assign(obj1, obj2, obj3)
+console.log(obj1)
+
+console.log(obj2)
+console.log(obj3)
+
+//4. Even the empty object will have prototype
+
+var emptyObj = {} //Object.create({})
+//console.log(emptyObj.__proto__)
+
+
+//5. if we wish to break the prototype chain
+//you need to pass null in your object create constructor
+
+var nullProtoype =  Object.create(null);
+
+//console.log(nullProtoype.__proto__)
