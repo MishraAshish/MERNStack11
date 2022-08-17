d0cd40551936b2372157a0fd741e5fdfb948040a
AshisMishra <ash.ishu025@gmail.com>
2022-08-17 20:51:05 +0530

    class

---
+++ ES6/class.js
@@ -0,0 +1,33 @@
+//classes :  classes with syntax similar to java, .net 
+//  -- are special type of functions in javascript that give us desired behaviour
+
+// class SomeName {
+//     constructor(){
+
+//     }
+//     someMethods(){
+
+//     }
+// }
+
+class Vaccination 
+{
+    constructor(name, date, place) {
+        this.name = name;
+        this.date = date;
+        this.place = place;
+    }
+
+    getSchedule () {
+        return {
+            name : this.name,
+            place : this.place
+        }
+    }
+}
+
+let firstDose = new Vaccination("RCL-First ","18th August", "In some hospital")
+
+console.log(firstDose.getSchedule())
+
+//create a class to calculate area of anything
