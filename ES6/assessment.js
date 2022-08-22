f1de3d8c2c7d2736fdde410eb30310282ac5e066
AshisMishra <ash.ishu025@gmail.com>
2022-08-22 19:43:49 +0530

    assessment questions

---
+++ ES6/assessment.js
@@ -0,0 +1,47 @@
+// 1. How to preserve the immutability on my heroes list? Solve below problems using the same
+// a. Get heroes who are not evils
+// b. Print Unique family names
+// c. Print Hero Names from given objects, and append sir in each of them before printing
+// d. Do we have any hero in Marvel Family who is not evil
+// e. 
+
+// const heroes = [
+//   { name: 'Wolverine',      family: 'Marvel',    isEvil: false },
+//   { name: 'Deadpool',       family: 'Marvel',    isEvil: false },
+//   { name: 'Magneto',        family: 'Marvel',    isEvil: true  },
+//   { name: 'Charles Xavier', family: 'Marvel',    isEvil: false },
+//   { name: 'Batman',         family: 'DC Comics', isEvil: false },
+//   { name: 'Harley Quinn',   family: 'DC Comics', isEvil: true  },
+//   { name: 'Legolas',        family: 'Tolkien',   isEvil: false },
+//   { name: 'Gandalf',        family: 'Tolkien',   isEvil: false },
+//   { name: 'Saruman',        family: 'Tolkien',   isEvil: true  }
+// ]
+
+//2. Give me an example of map and set collection each with at least four properties implemented - like get, set, clear, etc
+
+//3. Create a promise object that get resloved after two seconds and rejected after three. Also it returns five ES6 features on resolved
+
+//4. Using the promise object at #3 create an async and await feature
+
+//5. Use the spread and rest operator to create a function which can multiple numbers from 1...n (n is the number of choice)
+//   also need to print students of the session using same example
+
+//6. Print the last name through destructuring and add a contact number:9119119110 as well
+// const person = {
+//     userDetails :{
+//         first: "FirstName",
+//         last: "LastName"
+//     }
+// }
+
+//7. Give me an example of const data manipulation
+
+//8. What is the difference between for-of and for-in show with examples
+
+//9. There is a task where we need to make three server calls - named as signin, userregistration and nextpageToNavigate
+//   all of them should contain a status code and status success or failed
+//   Create three promises for the same and we need to make sure that we move the user to next page only when all the calls are succeeded
+
+//10. In question number 9 we need to check the status of each promise and we can move to the next page even if nextpageToNavigate 
+//   call gets failed however nothing should be done if first to fails, give me an example of cocurrent promises
+//   with which we can achieve such scenarios
