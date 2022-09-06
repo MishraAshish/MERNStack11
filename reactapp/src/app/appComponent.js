4aa425d472bcebf8466bcb725c38324fb406cc78
AshisMishra <ash.ishu025@gmail.com>
2022-09-06 19:59:21 +0530

    setup with webpack 5

--- reactapp/src/app/appComponent.js
+++ reactapp/src/app/appComponent.js
@@ -5,7 +5,7 @@ export default class AppComponent extends Component {
 
     render(){
         let User = {
-            name : "Smart Student"
+            name : "MERNStack WebPack 5"
         }
         //we'll write our html + js in JSX (javascript as xml) format as below
         //it must return only one parent
@@ -14,6 +14,7 @@ export default class AppComponent extends Component {
                 <h1>This is Application Component</h1>
                 <div><b>{User.name}</b></div>
                 <h1>This will hold our Shopping Cart Application</h1>
+                <input type={"text"} value="Enter Name"></input>
             </>            
         )        
     }
