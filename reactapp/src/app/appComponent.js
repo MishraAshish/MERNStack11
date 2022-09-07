153f1a9d3a025eb1fd89dc38c560a6d6b67e9565
AshisMishra <ash.ishu025@gmail.com>
2022-09-07 20:31:19 +0530

    react first component

--- reactapp/src/app/appComponent.js
+++ reactapp/src/app/appComponent.js
@@ -3,17 +3,36 @@ import React, { Component } from "react"; //React - is default import and Compon
 
 export default class AppComponent extends Component {
 
+    constructor(props, context){
+        super(props);
+
+        this.state = {
+            Title : "This is initial title"
+        }
+    }
+
+    changeName = ()=>{
+        console.log("Test")
+        this.setState({
+            Title : "Updated Title on click!!"
+        })
+    }
+
     render(){
+        console.log("Appcomponent Rendered!!!")
         let User = {
-            name : "MERNStack WebPack 5"
+            name : "MERNStack WebPack 6"
         }
         //we'll write our html + js in JSX (javascript as xml) format as below
-        //it must return only one parent
+        //it must return only one parent //it is also termed as virtual DOM
         return(
             <>
                 <h1>This is Application Component</h1>
                 <div><b>{User.name}</b></div>
-                <h1>This will hold our Shopping Cart Application</h1>
+                
+                <h2>{this.state.Title}</h2>
+
+                <button onClick={this.changeName}>Click To change Title</button>
                 <input type={"text"} value="Enter Name"></input>
             </>            
         )        
