1b59143cb67f51ab981ba568058bde67c8ab2967
AshisMishra <ash.ishu025@gmail.com>
2022-09-22 21:31:24 +0530

    question

---
+++ reactapp/src/app/Common/ProjectEnhancement.js
@@ -0,0 +1,15 @@
+// 22-09-2022 - Practice Question 1 : Product
+
+// Create a product component using functional component and hooks (try using use state to read default values from reducer)
+// Create a form to allow user to submit Product Details - name, price, desc, rating
+// Create an action method to add the detail to database using a server api
+// Server Side - Create product router and api to save the product using productdatamodel
+
+
+
+
+// Other Practice Questions 
+// Practice - 2
+
+//1. Create a component with name Hobby, add a textbox to insert hobbyname into it and save it to the database using SaveAPI
+//2. Create an action to save the hobby, can use useractions file and userrouter file for the same
---
+++ reactapp/src/app/Common/ReactCodingTest.js
@@ -0,0 +1,21 @@
+//Coding Test 1
+
+// create a class/functional component with state naming - Tom
+// create react ref elements likes - name, address, age and seesionName
+// use ref elements to create uncontrolled component
+// upon form submit set the values from these components to the state
+// show the user typed values from state to multiple labels - (show these labels anywhere on the page)
+// list down benefits of controlled over uncontrolled components
+
+//Coding Test 2
+// create navigations for your own name page (Tom)
+// also move to that page on button click
+// make a route with path params in it - (Tom/25)
+
+//Coding Test 3
+// Create a component with your name (first)
+// Show all lifecycle methods that have been discussed
+// Create another example with Pure component
+// Make that pure component child of first component
+// Pass a event/function as a prop to Child component and use it to send data back, and show update 
+// use ref keyword to access one element in first component
